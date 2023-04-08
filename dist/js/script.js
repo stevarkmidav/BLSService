520e49172e5014d6abce9ff1096f7a1884d35357
Vadym <stevarkmidav@gmail.com>
2023-04-08 14:04:36 +0300

    fix swiper speed

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,29 @@
 window.addEventListener('DOMContentLoaded', () => {
   //swiper
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // Pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 600,
+    
+    // Pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
     },
-  
-  autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-});
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      },
+    
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+  });
+
 
   // gallery 
   Fancybox.bind('[data-fancybox="gallery"]', {
